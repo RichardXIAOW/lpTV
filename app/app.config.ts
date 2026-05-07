70ca3d48df1fb05641a19d3b47d6272321a2b212
RichardXIAOW <2932257007@qq.com>
2026-05-07 23:14:54 +0800

    style: 增加颜色

---
+++ app/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'blue',
+    gray: 'neutral',
+    background: 'gray-900',
+    body: 'gray-900',
+    header: {
+      slots: {
+        root: 'bg-gray-900 border-b border-gray-700'
+      }
+    },
+  }
+})
