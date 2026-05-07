f8bea50dea9ac2193b0887b678745a1c57db5984
RichardXIAOW <2932257007@qq.com>
2026-05-07 22:45:09 +0800

    fix: 新增配置vercel

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <!-- Your code here -->
+        11
     </div>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  nitro: {
+    preset: 'vercel', // 用 Serverless（稳定）
+    // preset: 'vercel-edge', // 想用 Edge 就开这个
+  },
   modules: [
     '@nuxt/ui'
   ],
